27c98d5d372c39d5daa2bab3095df7378719cc00
Daniel Pérez Rada <dperezrada@gmail.com>
2018-12-03 19:42:49 -0300

    ADD: Improving the way the export is been done

--- nlp/keywords2vec/export_vectors.py
+++ nlp/keywords2vec/export_vectors.py
@@ -8,9 +8,9 @@ import gensim
 def main():
     parser = ArgumentParser()
     parser.add_argument(
-        "-d", "--directory", dest="experiment_path",
-        required=False, help="the directory where the word2vec and keywords_counter is located",
-        metavar="DIRECTORY", default="data/experiments/experiment_1"
+        "-v", "--vector_path", dest="vector_path",
+        required=True, help="the path to the .vec model",
+        metavar="VECTOR_MODEL_PATH"
     )
     parser.add_argument(
         "-o", "--output-directory", dest="output",
@@ -20,17 +20,19 @@ def main():
     parser.add_argument(
         "-s", "--size", dest="size",
         required=False, help="the number of vectors you wanted to export. Default to all",
-        metavar="DIRECTORY", default="all"
+        metavar="SIZE", default="all"
+    )
+    parser.add_argument(
+        "-p", "--prefix", dest="prefix",
+        required=False, help="prefix to pre-append to the file name",
+        metavar="PREFIX", default=""
     )
 
     args = parser.parse_args()
 
-    store_model_path = os.path.join(args.experiment_path, "word2vec.vec")
-    keywords_vectors = load_vectors(store_model_path)
+    keywords_vectors = load_vectors(args.vector_path)
 
-    counter_store_path = os.path.join(args.experiment_path, "keywords_counter.tsv.gz")
-    counter = load_counter(counter_store_path)
-    export_vector(keywords_vectors, counter, args.output, args.size)
+    export_vector(keywords_vectors, args.output, args.size, args.prefix)
 
 ## Load the vectors
 def load_vectors(store_model_path):
@@ -39,38 +41,30 @@ def load_vectors(store_model_path):
     )
     return keywords_vectors
 
-def load_counter(counter_path):
-    counter_dict = {}
-    for line in gzip.open(counter_path, "rt"):
-        keyword, total = line[0:-1].split("\t")
-        if keyword:
-            counter_dict[keyword] = int(total)
-    counter = Counter(counter_dict)
-    del counter_dict
-    return counter
-
-def export_vector(keywords_vectors, counter, output_dir, max_number):
-    words_path = os.path.join(output_dir, "words.tsv.gz")
-    vectors_path = os.path.join(output_dir, "vectors.tsv.gz")
+def export_vector(keywords_vectors, output_dir, max_number, prefix=""):
+    if prefix:
+        prefix = "%s_" % prefix
+    words_path = os.path.join(output_dir, "%swords.tsv" % prefix)
+    vectors_path = os.path.join(output_dir, "%svectors.tsv" % prefix)
     max_index = 0
     if max_number != "all":
         max_index = int(max_number)
     index = 0
-    with gzip.open(words_path, "wt") as file_words:
-        with gzip.open(vectors_path, "wt") as file_vec:
-            for keyword, _ in counter.most_common():
+    with open(words_path, "wt") as file_words:
+        with open(vectors_path, "wt") as file_vec:
+            for keyword in keywords_vectors.vocab.keys():
                 try:
-                    file_vec.write(
-                        "\t".join(
-                            (
-                                #[keyword,] +
-                                [
-                                    str(round(vector_i, 3))
-                                    for vector_i in keywords_vectors[keyword]
-                                ]
-                            )
-                        ) + "\n"
-                    )
+                    vector_str = "\t".join(
+                        (
+                            [
+                                str(round(vector_i, 3))
+                                for vector_i in keywords_vectors[keyword]
+                            ]
+                        )
+                    ) + "\n"
+                    if vector_str.find("nan") >= 0:
+                        continue
+                    file_vec.write(vector_str)
                     file_words.write(keyword.replace(" ", "_") + "\n")
                 except:
                     continue
